bb7e4c4da1597e4efe7c43caca82e0ed2722de13
abdoachhoubi <aachhoub@gmail.com>
2025-08-08 19:37:29 +0100

    remove unecessary files

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -79,21 +79,21 @@ const Navigation = () => {
     return (
       <nav className="flex justify-center fixed top-0 w-full z-50 items-center p-2">
         <ul
-          className={`${ibmPlexMono.className} flex gap-6 list-none p-3 bg-[rgba(0,0,0,0.2)] rounded-full items-center`}
+          className={`${ibmPlexMono.className} flex gap-6 list-none p-3 lg:p-2 xl:p-3 bg-[rgba(0,0,0,0.2)] rounded-full items-center`}
         >
           <Link href="https://abdoachhoubi.me" target="_blank" rel="noopener noreferrer">
-            <Image src="/logo.png" alt="Logo" width={40} height={40} className="rounded-full" />
+            <Image src="/logo.png" alt="Logo" width={40} height={40} className="rounded-full w-10 h-10" />
           </Link>
-          <li className="cursor-pointer hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
+          <li className="cursor-pointer lg:text-xs xl:text-sm 2xl:text-base hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
             <Link href="/">Home</Link>
           </li>
-          <li className="cursor-pointer hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
+          <li className="cursor-pointer lg:text-xs xl:text-sm 2xl:text-base hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
             <Link href="/website-builder">Website Builder</Link>
           </li>
-          <li className="cursor-pointer hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
+          <li className="cursor-pointer lg:text-xs xl:text-sm 2xl:text-base hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
             <Link href="/saas">Saas</Link>
           </li>
-          <li className="cursor-pointer hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
+          <li className="cursor-pointer lg:text-xs xl:text-sm 2xl:text-base hover:bg-[rgba(0,0,0,0.4)] px-4 py-2 rounded-full transition:all duration-200 ease-in-out">
             <Link href="/e-learning">E-Learning</Link>
           </li>
           <div className="flex items-center justify-center px-4">
